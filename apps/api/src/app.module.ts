33752d9d851f5cdf9d7370d890e9f0707f709deb
DeVx1978 <elgurudelfutbol2026@gmail.com>
2026-07-18 18:19:57 -0500

    confirmado en PC: busqueda y roles de seguridad funcionando

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './infraestructura/database/database.module';
 import { SaludController } from './presentacion/salud/salud.controller';
 import { AuthModule } from './presentacion/auth/auth.module';
+import { BusquedaModule } from './presentacion/busqueda/busqueda.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, AuthModule, BusquedaModule],
   controllers: [AppController, SaludController],
   providers: [AppService],
 })
